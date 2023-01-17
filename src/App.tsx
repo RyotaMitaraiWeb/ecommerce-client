80c9272e3096192bcd093f97c76fec4c5700cbb3
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-01-17 21:31:05 +0200

    add snackbar to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { CssBaseline, PaletteMode, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { useState, useMemo } from 'react';
+import { RouterProvider } from 'react-router-dom';
 import './App.scss';
+import SnackbarAlert from './features/snackbar/Snackbar';
+import { router } from './router';
 import { palette, themes } from './themes';
 
 function App() {
@@ -23,6 +26,8 @@ function App() {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
+            <RouterProvider router={router} />
+            <SnackbarAlert />
         </ThemeProvider>
     );
 }
