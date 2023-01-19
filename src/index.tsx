f9667ccc20f35c035d856051f4655121af778b4f
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-01-19 15:22:56 +0200

    restructure basic layout to properly display the router outlet

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,6 @@ const root = createRoot(container);
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <RouterProvider router={router} />
             <App />            
         </Provider>
     </React.StrictMode>
--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,29 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Outlet } from 'react-router-dom';
+import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
+
+export default function Layout() {
+    return (
+        <>
+            <Header />
+            <Outlet />
+        </>
+    )
+}
 
 export const router = createBrowserRouter([
     {
-        path: '/',
-        element: <Home />,
-    },
-    {
-        path: 'about',
-        element: <h1>About</h1>
+        element: <Layout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+                index: true,
+            },
+            {
+                path: 'about',
+                element: <h1>About</h1>
+            }
+        ]
     }
 ]);
