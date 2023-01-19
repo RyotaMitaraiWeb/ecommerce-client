86d570c173333d95a9dd7ae84a5dc7164cf5a5d4
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-01-19 15:19:47 +0200

    add test utils

---
+++ src/util/test-utils/test.utils.tsx
@@ -0,0 +1,24 @@
+import { render, RenderOptions } from "@testing-library/react";
+import { ReactElement } from "react";
+import { Provider } from "react-redux";
+import { MemoryRouter, Router, RouterProvider } from "react-router-dom";
+import App from "../../App";
+import { store } from "../../app/store";
+import { router } from "../../router";
+
+
+const allProviders = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    )
+};
+
+const customRender = (
+    ui: ReactElement,
+    options?: Omit<RenderOptions, 'wrapper'>,
+) => render(ui, { wrapper: allProviders, ...options })
+
+export * from '@testing-library/react'
+export { customRender as render }
