66f09fccc8ddc632717f71aca882c8b3eb71a2ba
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-01-20 23:26:57 +0200

    override MUI's Button Base so that it returns a React Router link

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { CssBaseline, PaletteMode, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
+import React from 'react';
 import { useState, useMemo } from 'react';
-import { RouterProvider } from 'react-router-dom';
+import { Link, LinkProps, RouterProvider } from 'react-router-dom';
 import './App.scss';
 import SnackbarAlert from './features/snackbar/Snackbar';
 import { router } from './router';
 import { palette, themes } from './themes';
+
+const LinkBehavior = React.forwardRef<
+    HTMLAnchorElement,
+    Omit<LinkProps, 'to'> & { href: LinkProps['to'] }
+>((props, ref) => {
+    const { href, ...other } = props;
+    // Map href (MUI) -> to (react-router)
+    return <Link ref={ref} to={href} {...other} />;
+});
 
 function App() {
     const [mode, setMode] = useState<PaletteMode>('dark');
@@ -18,10 +28,18 @@ function App() {
                 primary: {
                     main: themes[palette]
                 }
+            },
+            components: {
+                MuiButtonBase: {
+                    defaultProps: {
+                        LinkComponent: LinkBehavior,
+                    },
+                }
             }
-        }), [mode, palette]
+        }
+        ), [mode, palette]
     );
-    
+
 
     return (
         <ThemeProvider theme={theme}>
