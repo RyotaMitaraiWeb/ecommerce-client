2ed73b2974687cf743337d8477457fe34f4cad00
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-02-04 23:01:24 +0200

    update redirectViaStatus documentation

--- src/util/requests/redirect.ts
+++ src/util/requests/redirect.ts
@@ -6,11 +6,12 @@ import { HttpStatus } from "../httpstatus.enum";
  * By returning it in the loader, it will redirect the user to specific pages, depending on status code:
  * * 404 - to a "not-found" page
  * * 403 - to the home page
+ * * 404 - to the login page, if ``redirectIfGuess`` is ``true``
  * * everything else - no redirect (returns ``null``)
  * 
  * When the function returns ``null``, this will allow the loader to proceed with the rendering.
  * If you want to use this function and also load data upon success, 
- * use the logical OR operator (``||``) to return either null or the data to be loaded
+ * use the logical OR operator (``||``) to return either ``null`` or the data to be loaded
  * @param {number} status - status code of the response
  * @returns 
  */
